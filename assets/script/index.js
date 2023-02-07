fb4ee2a27d4d5f2b62642a42c4e631f6c649e038
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2023-02-07 09:23:18 -0300

    Melhorei a lógica do js

--- assets/script/index.js
+++ assets/script/index.js
@@ -7,18 +7,22 @@ let buttom = document.querySelector('#buttom')
 const calcular = () => {
     let imc = (peso.value/altura.value**2)
 
-    if(imc < 18.5){
-        resultado.innerHTML = `
-        ${nome.value} seu IMC é ${imc.toFixed(2)} e você está abaixo do peso`;
-    } else if(imc < 24.99){
-        resultado.innerHTML = `
-        ${nome.value} seu IMC é ${imc.toFixed(2)} e seu peso está normal`;
-    } else if(imc < 29.99){
-        resultado.innerHTML = `
-        ${nome.value} seu IMC é ${imc.toFixed(2)} e você está no sobrepeso`;
+    if(nome.value != '' && altura.value != '' && peso.value != ''){
+        if(imc < 18.5){
+            resultado.innerHTML = `
+            ${nome.value} seu IMC é ${imc.toFixed(2)} e você está abaixo do peso`;
+        } else if(imc < 24.99){
+            resultado.innerHTML = `
+            ${nome.value} seu IMC é ${imc.toFixed(2)} e seu peso está normal`;
+        } else if(imc < 29.99){
+            resultado.innerHTML = `
+            ${nome.value} seu IMC é ${imc.toFixed(2)} e você está no sobrepeso`;
+        } else {
+            resultado.innerHTML = `
+            ${nome.value} seu IMC é ${imc.toFixed(2)} e você está na obesidade`;
+        }
     } else {
-        resultado.innerHTML = `
-        ${nome.value} seu IMC é ${imc.toFixed(2)} e você está na obesidade`;
+        resultado.innerHTML = 'Preencha todos os dados'
     }
 }
 
